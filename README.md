b43458cafb51f59a867c7ce88bdd062d909ab39d
Mario Cueva <mario.cueva.au@gmail.com>
2017-08-24 20:44:23 +1000

    finished challenge

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Sum-All-Numbers-in-a-Range
 Free Code Camp challenge to 'Sum All Numbers in a Range'
+
+We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+The lowest number will not always come first.
--- Sum-All-Numbers-in-a-Range.js
+++ Sum-All-Numbers-in-a-Range.js
@@ -4,8 +4,20 @@
 // The lowest number will not always come first.
 
 function sumAll(arr) {
-    return 1;
+  var min = Math.min(arr[0], arr[1]);
+  var max = Math.max(arr[0], arr[1]);
+
+  for (i = min+1; i <= max-1; i++) {
+    arr.push(i);
   }
-  
-  sumAll([1, 4]);
-  
+
+  return arr.reduce( (previousVal, currentVal) => previousVal + currentVal);
+}
+
+// sumAll([1, 4]);
+
+console.log(sumAll([1, 4])); //should return a number.
+console.log(sumAll([1, 4])); //should return 10.
+console.log(sumAll([4, 1])); //should return 10.
+console.log(sumAll([5, 10])); //should return 45.
+console.log(sumAll([10, 5])); //should return 45.
